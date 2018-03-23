6eac29defddc8334747fbfa6b3659384356f9e1e
Alvin Lu <alvinspeakstoo@gmail.com>
2018-03-22 19:58:31 -0700

    made site more responsive

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'a/A' });
 });
 
+router.get('/responsive', function(req, res, next) {
+  res.render('responsive', { title: 'a/A' });
+});
 module.exports = router;
